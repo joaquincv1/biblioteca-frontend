61c406c8dfd076894cadfa0d7dd1c6e7223d74b6
Joaquin Castillo <joaquinacv04@gmail.com>
2025-11-05 14:46:12 -0300

    feat: configuracion final y correccion de CORS para el despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
-// Este archivo se usará automáticamente con 'ng build'
 export const environment = {
-    production: true,
-    // Aún no tenemos esta URL, pero ya la dejamos lista.
-    // Esta será la URL de tu backend en Render.
-    apiUrl: 'https://tu-api-en-render.onrender.com/api' 
+    production: true, 
+    //  Render.
+    apiUrl: 'https://backend-biblioteca-4pm7.onrender.com/api' 
 };
